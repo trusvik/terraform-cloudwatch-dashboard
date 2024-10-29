c4f59081bb8fab2d22855ae625e66d7cd7b65d73
EC2 Default User <ec2-user@ip-172-31-43-60.eu-west-1.compute.internal>
2024-10-29 10:26:12 +0000

    added updated code/ initial commit

--- src/main/java/com/example/demo/MetricsConfig.java
+++ src/main/java/com/example/demo/MetricsConfig.java
@@ -35,7 +35,7 @@ public class MetricsConfig {
     private CloudWatchConfig setupCloudWatchConfig() {
         return new CloudWatchConfig() {
             private Map<String, String> configuration = Map.of(
-                    "cloudwatch.namespace", "",
+                    "cloudwatch.namespace", "toru010",
                     "cloudwatch.step", Duration.ofSeconds(5).toString());
 
             @Override
